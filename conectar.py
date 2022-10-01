a7e6aef1fb6a4a82f4e1cb0536b05056d06caa75
eze <106458263+eze2013@users.noreply.github.com>
2022-09-30 23:50:17 -0300

    conectando a la base de datos

---
+++ conectar.py
@@ -0,0 +1,17 @@
+import mysql.connector 
+from mysql.connector import Error
+
+try:
+    conexion = mysql.connector.connect (user='root', 
+                                        password = 'Locura20',  # utilizar la contraseña de mysql
+                                        host = 'localhost', 
+                                        database = 'disqueria',
+                                        port = '3306' )
+    if conexion.is_connected():     # pregunta si esta conectado
+        print("Conexion Exitosa")
+except Error as ex:
+    print("Algo fallo en la conexion: ",ex) # por si algo fallo
+finally: 
+    if conexion.is_connected():
+        conexion.close()  #cierra la conexion con la base de datos
+        print("La conexion a finalizado")
